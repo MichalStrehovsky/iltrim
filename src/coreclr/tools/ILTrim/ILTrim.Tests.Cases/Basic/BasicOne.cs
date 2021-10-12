9449a22b4a6aca9966f13376f3e63035807f3dcb
vitek-karas <vitek.karas@microsoft.com>
2021-10-12 05:30:14 -0700

    Test discovery

---
+++ src/coreclr/tools/ILTrim/ILTrim.Tests.Cases/Basic/BasicOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILTrim.Tests.Cases.Basic
+{
+    internal class BasicOne
+    {
+    }
+}
