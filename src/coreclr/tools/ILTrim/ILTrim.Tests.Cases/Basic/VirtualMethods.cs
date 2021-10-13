f04088a12df4b05f1dccca193dd30815f6ed86cc
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2021-10-13 16:56:28 +0900

    Track virtual method usage
    This adds tracking virtual methods to ensure that if a type was allocated and provides an implementation to a virtual method that was used, we will generate the implementation as well.
    
    Introduced a new node type that represents a constructed type, and another node type that represents a virtual method use. The constructed type node conditionally depends on virtual method implementations, conditioned on the virtual method being used.

---
+++ src/coreclr/tools/ILTrim/ILTrim.Tests.Cases/Basic/VirtualMethods.cs
@@ -0,0 +1,41 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Basic
+{
+#pragma warning disable 169
+
+    [Kept]
+    public class VirtualMethods
+    {
+        [Kept]
+        static void Main()
+        {
+            BaseType b = new DerivedType();
+            b.Method1();
+
+            ((object)default(MyValueType)).ToString();
+        }
+    }
+
+    [Kept]
+    class BaseType
+    {
+        [Kept]
+        public virtual void Method1() { }
+        public virtual void Method2() { }
+    }
+
+    class DerivedType : BaseType
+    {
+        [Kept]
+        public override void Method1() { }
+        public override void Method2() { }
+    }
+
+    [Kept]
+    struct MyValueType
+    {
+        [Kept]
+        public override string ToString() => "";
+    }
+}
