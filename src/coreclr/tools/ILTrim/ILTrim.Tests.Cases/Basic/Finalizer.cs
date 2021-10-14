c84a6be35b73c5f293ab1a2e4c37c23e29010bdb
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2021-10-14 15:08:35 +0900

    Ensure the finalizer is kept
    Consider finalizer an implicitly used virtual.

---
+++ src/coreclr/tools/ILTrim/ILTrim.Tests.Cases/Basic/Finalizer.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using System;
+
+namespace Mono.Linker.Tests.Cases.Basic
+{
+    [Kept]
+    class Finalizer
+    {
+        static void Main()
+        {
+            MentionUnallocatedType(null);
+            new AllocatedTypeWithFinalizer();
+        }
+
+        static void MentionUnallocatedType(UnallocatedTypeWithFinalizer u) { }
+
+        [Kept]
+        class UnallocatedTypeWithFinalizer
+        {
+            // Not kept
+            ~UnallocatedTypeWithFinalizer() { }
+        }
+
+        [Kept]
+        class AllocatedTypeWithFinalizer
+        {
+            [Kept]
+            ~AllocatedTypeWithFinalizer() { }
+        }
+    }
+}
--- src/coreclr/tools/ILTrim/ILTrim/Trimmer.cs
+++ src/coreclr/tools/ILTrim/ILTrim/Trimmer.cs
@@ -8,6 +8,7 @@ using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
 
+using Internal.TypeSystem;
 using Internal.TypeSystem.Ecma;
 
 using ILCompiler.DependencyAnalysisFramework;
@@ -42,7 +43,12 @@ namespace ILTrim
 
             var analyzer = new DependencyAnalyzer<NoLogStrategy<NodeFactory>, NodeFactory>(factory, resultSorter: null);
             MethodDefinitionHandle entrypointToken = (MethodDefinitionHandle)MetadataTokens.Handle(module.PEReader.PEHeaders.CorHeader.EntryPointTokenOrRelativeVirtualAddress);
+
             analyzer.AddRoot(factory.MethodDefinition(module, entrypointToken), "Entrypoint");
+
+            analyzer.AddRoot(factory.VirtualMethodUse(
+                (EcmaMethod)context.GetWellKnownType(WellKnownType.Object).GetMethod("Finalize", null)),
+                "Finalizer");
 
             analyzer.ComputeMarkedNodes();
 
